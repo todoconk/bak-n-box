a87ed524eaa5a92c3d57def8cdefd2fd2f78cdcf
todoconk <todoconk@bitbucket.org>
2014-01-24 14:36:54 -0430

    -

--- init.php
+++ init.php
@@ -1,9 +1,9 @@
 <?php 
-    require_once 'config.php';
-    require_once 'phpMySQLBackup.php';
-    require_once 'dropboxupload.php';
+require_once 'config.php';
+require_once 'phpMySQLBackup.php';
+require_once 'dropboxupload.php';
 
-    $filename = backup_tables(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME);
-    upload_dropbox($dropbox, APP_DIR_PATH . $filename, sprintf('%s/%s/%s/%s', APP_DOMAIN, date('Y'), date('m'), $filename ));
+$filename = backup_tables(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME);
+$result = upload_dropbox($dropbox, APP_DIR_PATH . $filename, sprintf('%s/%s/%s/%s', APP_DOMAIN, date('Y'), date('m'), $filename ));
 
-    header('Location: index.php');
+print_r($result);

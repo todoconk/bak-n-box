a9e1054bfaa13e465ad1d120cf7d940867e868e3
todoconk <todoconk@bitbucket.org>
2014-01-24 17:45:17 -0430

    add database name to url

--- init.php
+++ init.php
@@ -4,6 +4,6 @@ require_once 'phpMySQLBackup.php';
 require_once 'dropboxupload.php';
 
 $filename = backup_tables(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME);
-$result = upload_dropbox($dropbox, APP_DIR_PATH . $filename, sprintf('%s/%s/%s/%s', APP_DOMAIN, date('Y'), date('m'), $filename ));
+$result = upload_dropbox($dropbox, APP_DIR_PATH . $filename, sprintf('%s/%s/%s/%s/%s', APP_DOMAIN, DATABASE_NAME, date('Y'), date('m'), $filename ));
 
 print_r($result);
